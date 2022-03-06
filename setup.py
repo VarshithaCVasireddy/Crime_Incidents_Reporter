da80c628443811029a0865bab7bf9a208ee3f87c
Varshitha Vasireddy <varshithavasireddy@gmail.com>
2022-03-06 15:26:10 +0000

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='project0',
+	version='1.0',
+	author='Varshitha Choudary Vasireddy',
+	author_email='varshitha.c.vasireddy@ou.edu',
+	packages=find_packages(exclude=('tests', 'docs')),
+	setup_requires=['pytest-runner'],
+	tests_require=['pytest']	
+)
+
